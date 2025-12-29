cef01b3682a201e87c02d656c7fe13a69a8b8f59
Hutch79 <me@hutch79.ch>
2025-12-29 21:56:33 +0100

    Add wireguard support

--- configuration.nix
+++ configuration.nix
@@ -116,7 +116,8 @@
   };
 
   services.qemuGuest.enable = true;
-  
+  networking.wireguard.enable = true;
+
   # Boot configuration
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
